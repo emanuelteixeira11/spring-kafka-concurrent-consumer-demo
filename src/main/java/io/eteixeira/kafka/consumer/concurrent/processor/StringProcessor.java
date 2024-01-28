28d4b218cfe75ac74fcdf4b557096a64d1dea6c0
eteixeira <em.teixeira1994@gmail.com>
2024-01-28 17:56:46 +0100

    Added javadocs and refactored test class

--- src/main/java/io/eteixeira/kafka/consumer/concurrent/processor/StringProcessor.java
+++ src/main/java/io/eteixeira/kafka/consumer/concurrent/processor/StringProcessor.java
@@ -12,6 +12,11 @@ import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * Processor class for handling string data.
+ * This class is responsible for processing and storing string values.
+ * It is scoped as a prototype bean in the Spring context.
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -19,14 +24,23 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class StringProcessor {
 
     private final String id;
-
     private final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
 
+    /**
+     * Initializes the StringProcessor instance.
+     * This method logs the creation of a new StringProcessor instance with its unique ID.
+     */
     @PostConstruct
     public void init() {
         log.info("Initializing new String Processor with id: {}", this.id);
     }
 
+    /**
+     * Processes and stores a string value in a queue.
+     * Logs the processing action and handles any potential interruption during the process.
+     *
+     * @param value The string value to be processed.
+     */
     public void processString(String value) {
         log.info("Instance {} processing new string {}.", this.id, value);
         try {
@@ -37,11 +51,22 @@ public class StringProcessor {
         }
     }
 
+    /**
+     * Retrieves the current size of the queue.
+     *
+     * @return The number of elements in the queue.
+     */
     public int queueSize() {
         return this.queue.size();
     }
 
-    public Set<String> queuedData() {
+    /**
+     * Returns a set of distinct strings from the queue.
+     * This method is useful for retrieving all unique values that have been processed.
+     *
+     * @return A Set of distinct strings.
+     */
+    public Set<String> distinctQueuedData() {
         return new HashSet<>(this.queue);
     }
 }
--- src/test/java/io/eteixeira/kafka/consumer/concurrent/SpringKafkaDemoApplicationTests.java
+++ src/test/java/io/eteixeira/kafka/consumer/concurrent/SpringKafkaDemoApplicationTests.java
@@ -30,70 +30,109 @@ import java.util.stream.IntStream;
 
 import static io.eteixeira.kafka.consumer.concurrent.listener.SpringKafkaListener.SPRING_KAFKA_TEST_TOPIC;
 
+/**
+ * Tests for the Spring Kafka Demo Application.
+ * This class includes tests to verify the proper functioning of KafkaTemplate and StringProcessor beans.
+ */
 @SpringBootTest(classes = {SpringKafkaDemoApplicationTests.TestKafkaDemoApplicationConfiguration.class, SpringKafkaDemoApplication.class})
 class SpringKafkaDemoApplicationTests {
 
-	@Autowired
-	private KafkaTemplate<Object, Object> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<Object, Object> kafkaTemplate;
 
-	@Autowired
-	@Qualifier("stringProcessors")
-	private List<StringProcessor> stringProcessors;
+    @Autowired
+    @Qualifier("stringProcessors")
+    private List<StringProcessor> stringProcessors;
+    private static final int NUM_MESSAGES = 1000;
+    private static final int PARTITION_KEY_DIVISOR = 10;
 
-	@Test
-	void all_messages_processed_by_multiple_string_processor_instances() {
-		Assertions.assertThat(kafkaTemplate).isNotNull();
-		var futures = IntStream.range(0, 1000)
-				.mapToObj(value -> "key_" + value % 10)
-				.map(key -> this.kafkaTemplate.send(SPRING_KAFKA_TEST_TOPIC, key, "data for key: " + key))
-						.toArray(CompletableFuture[]::new);
-		Assertions.assertThat(CompletableFuture.allOf(futures)).succeedsWithin(Duration.ofSeconds(10));
-		Awaitility.await().untilAsserted(() -> Assertions.assertThat(this.stringProcessors).hasSize(2));
-		Assertions.assertThat(stringProcessors.getFirst().queueSize()).isPositive();
-		Assertions.assertThat(stringProcessors.getLast().queueSize()).isPositive();
-		Assertions.assertThat(stringProcessors.getLast().queuedData())
-				.doesNotContainAnyElementsOf(stringProcessors.getFirst().queuedData());
-	}
+    /**
+     * Tests if all messages are processed by multiple StringProcessor instances.
+     * Ensures that the KafkaTemplate is not null, sends a number of messages, and
+     * then verifies that all messages are processed within a specified duration and
+     * that each StringProcessor instance has a non-empty queue with distinct data.
+     */
+    @Test
+    void testAllMessagesProcessedByMultipleStringProcessors() {
+        Assertions.assertThat(kafkaTemplate).isNotNull();
+        CompletableFuture<?>[] futures = sendMessages();
+        assertAllMessagesProcessed(futures);
+        assertStringProcessorsBehaviors();
+    }
 
+    /**
+     * Sends a predefined number of messages to a Kafka topic.
+     * Each message has a key derived from a modulus operation and a specific data payload.
+     *
+     * @return an array of CompletableFuture objects representing the result of each send operation.
+     */
+    private CompletableFuture<?>[] sendMessages() {
+        return IntStream.range(0, NUM_MESSAGES)
+                .mapToObj(value -> "key_" + value % PARTITION_KEY_DIVISOR)
+                .map(key -> kafkaTemplate.send(SPRING_KAFKA_TEST_TOPIC, key, "data for key: " + key))
+                .toArray(CompletableFuture[]::new);
+    }
 
-	@TestConfiguration
-	public static class TestKafkaDemoApplicationConfiguration {
+    /**
+     * Asserts that all sent messages are processed within a specific time frame.
+     *
+     * @param futures an array of CompletableFuture objects representing the result of each send operation.
+     */
+    private void assertAllMessagesProcessed(CompletableFuture<?>[] futures) {
+        Assertions.assertThat(CompletableFuture.allOf(futures)).succeedsWithin(Duration.ofSeconds(10));
+    }
 
-		@Bean("stringProcessors")
-		List<StringProcessor> stringProcessors() {
-			return new ArrayList<>();
-		}
+    /**
+     * Asserts specific behaviors of the StringProcessor instances.
+     * Verifies that there are exactly two StringProcessor instances, each with a non-empty queue,
+     * and that the data in the queues of these two processors are distinct.
+     */
+    private void assertStringProcessorsBehaviors() {
+        Awaitility.await().untilAsserted(() -> Assertions.assertThat(stringProcessors).hasSize(2));
+        Assertions.assertThat(stringProcessors.get(0).queueSize()).isPositive();
+        Assertions.assertThat(stringProcessors.get(1).queueSize()).isPositive();
+        Assertions.assertThat(stringProcessors.get(1).distinctQueuedData())
+                .doesNotContainAnyElementsOf(stringProcessors.get(0).distinctQueuedData());
+    }
 
-		@Bean
-		BeanPostProcessor beanPostProcessor(@Qualifier("stringProcessors") List<StringProcessor> stringProcessors) {
-			return new BeanPostProcessor() {
-				@Override
-				public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
-					if (bean instanceof StringProcessor stringProcessor) {
-						stringProcessors.add(stringProcessor);
-					}
-					return bean;
-				}
-			};
-		}
+    @TestConfiguration
+    public static class TestKafkaDemoApplicationConfiguration {
 
-		@Bean
-		@ServiceConnection
-		KafkaContainer kafkaContainer() {
-			return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
-		}
+        @Bean("stringProcessors")
+        List<StringProcessor> stringProcessors() {
+            return new ArrayList<>();
+        }
 
-		@Bean
-		Function<MessageListenerContainer, String> threadNameSupplier() {
-			return messageListenerContainer -> messageListenerContainer.getGroupId() + "-" +
-							Arrays.asList(messageListenerContainer.getListenerId().split("-")).getLast();
-		}
+        @Bean
+        BeanPostProcessor beanPostProcessor(@Qualifier("stringProcessors") List<StringProcessor> stringProcessors) {
+            return new BeanPostProcessor() {
+                @Override
+                public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+                    if (bean instanceof StringProcessor stringProcessor) {
+                        stringProcessors.add(stringProcessor);
+                    }
+                    return bean;
+                }
+            };
+        }
 
-		@Bean
-		NewTopic springKafkaTestTopic() {
-			return TopicBuilder.name(SPRING_KAFKA_TEST_TOPIC)
-					.partitions(5)
-					.build();
-		}
-	}
+        @Bean
+        @ServiceConnection
+        KafkaContainer kafkaContainer() {
+            return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+        }
+
+        @Bean
+        Function<MessageListenerContainer, String> threadNameSupplier() {
+            return messageListenerContainer -> messageListenerContainer.getGroupId() + "-" +
+                    Arrays.asList(messageListenerContainer.getListenerId().split("-")).getLast();
+        }
+
+        @Bean
+        NewTopic springKafkaTestTopic() {
+            return TopicBuilder.name(SPRING_KAFKA_TEST_TOPIC)
+                    .partitions(5)
+                    .build();
+        }
+    }
 }
